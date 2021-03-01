543648a5235e8d0f03e571ff998135077a9b4064
singseraphim <35641086+singseraphim@users.noreply.github.com>
2021-02-28 21:17:28 -0700

    Handler for adding a camera trap to a project

---
+++ src/main/java/ghostcat/capstone/add_camera_trap/AddCameraTrapDAO.java
@@ -0,0 +1,43 @@
+package ghostcat.capstone.add_camera_trap;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
+public class AddCameraTrapDAO {
+
+    //Objects used to access DynamoDB
+    static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+            .withRegion(Regions.US_EAST_1)
+            .build();
+    static DynamoDB dynamoDB = new DynamoDB(client);
+
+    //Name of table in DynamoDB
+    static String CAMERA_TABLE = "CameraTraps";
+
+    /**
+     * Performs operation on database
+     */
+    public AddCameraTrapResponse addCameraTrap(AddCameraTrapRequest request) {
+        //Create new response object
+        AddCameraTrapResponse response = new AddCameraTrapResponse();
+
+        //Get dynamoDB table
+        Table cameraTable = dynamoDB.getTable(CAMERA_TABLE);
+
+        //Create item to add to table
+        Item item = new Item().withPrimaryKey("ProjectID", request.projectID)
+                .withString("CameraTrapID", request.cameraTrapID)
+                .withDouble("camera_lat", request.cameraLat)
+                .withDouble("camera_lng", request.cameraLong);
+
+        //Add item to table
+        cameraTable.putItem(item);
+
+        return response;
+    }
+
+}
---
+++ src/main/java/ghostcat/capstone/add_camera_trap/AddCameraTrapResponse.java
@@ -0,0 +1,7 @@
+package ghostcat.capstone.add_camera_trap;
+
+public class AddCameraTrapResponse {
+    public String errorMsg = "";
+    public Boolean success = true;
+}
+
