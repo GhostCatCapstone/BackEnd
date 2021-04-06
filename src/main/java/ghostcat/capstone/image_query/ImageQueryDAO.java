efdcc493921855f6db597ee3fa961e7c8c9991c5
jeffersonbarney <jefferson.barney@icloud.com>
2021-04-05 20:32:04 -0600

    Updates to image query for new database

--- src/main/java/ghostcat/capstone/image_query/ImageQueryDAO.java
+++ src/main/java/ghostcat/capstone/image_query/ImageQueryDAO.java
@@ -11,10 +11,10 @@ import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
-import ghostcat.capstone.holders.Image;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class ImageQueryDAO {
 
@@ -24,8 +24,7 @@ public class ImageQueryDAO {
     static DynamoDB dynamoDB = new DynamoDB(client);
     static String BBOX_TABLE = "BoundingBoxes";
     static String PROJECT_TABLE = "ProjectData";
-    static String BBOX_DATE_INDEX = "UserID-img_date-index";
-    static String BBOX_CAMERA_TRAP_INDEX = "UserID-camera_trap-index";
+    static String BBOX_IMG_ID_INDEX = "UserID-img_id-index";
     static String IMAGE_TABLE = "Images";
     static String IMAGE_DATE_INDEX = "UserID-img_date-index";
     static String IMAGE_CAMERA_TRAP_INDEX = "UserID-camera_trap-index";
@@ -58,87 +57,50 @@ public class ImageQueryDAO {
                         .withString(":v_userID", request.userID)
                 );
         ItemCollection<QueryOutcome> items = userDataTable.query(spec);
-        Iterator<Item> iterator = items.iterator();
-        while (iterator.hasNext()) results.add(iterator.next());
+        for (Item item : items) {
+            results.add(item);
+        }
         return results;
     }
 
-    public ArrayList<Item> queryBBoxOnCameraTraps(ImageQueryRequest request) {
-        ArrayList<Item> results = new ArrayList<>();
+    public List<Item> queryBBoxOnImageId(String userID, String imageId) {
+        List<Item> results = new ArrayList<>();
 
         Table bboxTable = dynamoDB.getTable(BBOX_TABLE);
-        Index cameraTrapIndex = bboxTable.getIndex(BBOX_CAMERA_TRAP_INDEX);
+        Index imgIdIndex = bboxTable.getIndex(BBOX_IMG_ID_INDEX);
 
-        for (String cameraTrap : request.cameraTraps) {
-            String keyExp = "UserID = :v_userID and camera_trap = :v_cameraTrap";
-            ValueMap values = new ValueMap()
-                    .withString(":v_userID", request.userID)
-                    .withString(":v_cameraTrap", cameraTrap);
-            QuerySpec spec = new QuerySpec()
-                    .withKeyConditionExpression(keyExp)
-                    .withValueMap(values);
 
-            ItemCollection<QueryOutcome> items = cameraTrapIndex.query(spec);
-            Iterator<Item> iterator = items.iterator();
-            while (iterator.hasNext()) results.add(iterator.next());
+        String keyExp = "UserID = :v_userID and img_id = :v_imageId";
+        ValueMap values = new ValueMap()
+                .withString(":v_userID", userID)
+                .withString(":v_img_id", imageId);
+        QuerySpec spec = new QuerySpec()
+                .withKeyConditionExpression(keyExp)
+                .withValueMap(values);
+
+        ItemCollection<QueryOutcome> items = imgIdIndex.query(spec);
+        for (Item item : items) {
+            results.add(item);
         }
 
         return results;
     }
 
-    public ArrayList<Item> queryBBoxOnMinDate(ImageQueryRequest request) {
+    public ArrayList<Item> queryBBoxOnUserID(String userID) {
         ArrayList<Item> results = new ArrayList<>();
         Table bboxTable = dynamoDB.getTable(BBOX_TABLE);
-        Index dateIndex = bboxTable.getIndex(BBOX_DATE_INDEX);
-
-        String keyExp = "UserID = :v_userID and img_date >= :v_minDate";
-        ValueMap values = new ValueMap()
-                .withString(":v_userID", request.userID)
-                .withLong(":v_minDate", request.minDate);
-        QuerySpec spec = new QuerySpec()
-                .withKeyConditionExpression(keyExp)
-                .withValueMap(values);
-
-        ItemCollection<QueryOutcome> items = dateIndex.query(spec);
-        Iterator<Item> iterator = items.iterator();
-        while (iterator.hasNext()) results.add(iterator.next());
-        return results;
-    }
-
-    public ArrayList<Item> queryBBoxOnMaxDate(ImageQueryRequest request) {
-        ArrayList<Item> results = new ArrayList<>();
-        Table bboxTable = dynamoDB.getTable(BBOX_TABLE);
-        Index dateIndex = bboxTable.getIndex(BBOX_DATE_INDEX);
-
-        String keyExp = "UserID = :v_userID and img_date <= :v_maxDate";
-        ValueMap values = new ValueMap()
-                .withString(":v_userID", request.userID)
-                .withLong(":v_maxDate", request.maxDate);
-        QuerySpec spec = new QuerySpec()
-                .withKeyConditionExpression(keyExp)
-                .withValueMap(values);
-
-        ItemCollection<QueryOutcome> items = dateIndex.query(spec);
-        Iterator<Item> iterator = items.iterator();
-        while (iterator.hasNext()) results.add(iterator.next());
-        return results;
-    }
-
-    public ArrayList<Item> queryBBoxOnUserID(ImageQueryRequest request) {
-        ArrayList<Item> results = new ArrayList<>();
-        Table bboxTable = dynamoDB.getTable(BBOX_TABLE);
-        Index dateIndex = bboxTable.getIndex(BBOX_DATE_INDEX);
 
         String keyExp = "UserID = :v_userID";
         ValueMap values = new ValueMap()
-                .withString(":v_userID", request.userID);
+                .withString(":v_userID", userID);
         QuerySpec spec = new QuerySpec()
                 .withKeyConditionExpression(keyExp)
                 .withValueMap(values);
 
-        ItemCollection<QueryOutcome> items = dateIndex.query(spec);
-        Iterator<Item> iterator = items.iterator();
-        while (iterator.hasNext()) results.add(iterator.next());
+        ItemCollection<QueryOutcome> items = bboxTable.query(spec);
+        for (Item item : items) {
+            results.add(item);
+        }
         return results;
     }
 
@@ -156,8 +118,9 @@ public class ImageQueryDAO {
                 .withValueMap(values);
 
         ItemCollection<QueryOutcome> items = dateIndex.query(spec);
-        Iterator<Item> iterator = items.iterator();
-        while (iterator.hasNext()) results.add(iterator.next());
+        for (Item item : items) {
+            results.add(item);
+        }
         return results;
     }
 
@@ -175,8 +138,9 @@ public class ImageQueryDAO {
                 .withValueMap(values);
 
         ItemCollection<QueryOutcome> items = dateIndex.query(spec);
-        Iterator<Item> iterator = items.iterator();
-        while (iterator.hasNext()) results.add(iterator.next());
+        for (Item item : items) {
+            results.add(item);
+        }
         return results;
     }
 
@@ -196,28 +160,29 @@ public class ImageQueryDAO {
                     .withValueMap(values);
 
             ItemCollection<QueryOutcome> items = cameraTrapIndex.query(spec);
-            Iterator<Item> iterator = items.iterator();
-            while (iterator.hasNext()) results.add(iterator.next());
+            for (Item item : items) {
+                results.add(item);
+            }
         }
 
         return results;
     }
 
-    public ArrayList<Item> queryImagesOnUserID(ImageQueryRequest request){
+    public ArrayList<Item> queryImagesOnUserID(String userID){
         ArrayList<Item> results = new ArrayList<>();
         Table imgTable = dynamoDB.getTable(IMAGE_TABLE);
-        Index dateIndex = imgTable.getIndex(IMAGE_DATE_INDEX);
 
         String keyExp = "UserID = :v_userID";
         ValueMap values = new ValueMap()
-                .withString(":v_userID", request.userID);
+                .withString(":v_userID", userID);
         QuerySpec spec = new QuerySpec()
                 .withKeyConditionExpression(keyExp)
                 .withValueMap(values);
 
-        ItemCollection<QueryOutcome> items = dateIndex.query(spec);
-        Iterator<Item> iterator = items.iterator();
-        while (iterator.hasNext()) results.add(iterator.next());
+        ItemCollection<QueryOutcome> items = imgTable.query(spec);
+        for (Item item : items) {
+            results.add(item);
+        }
         return results;
     }
 }
--- src/main/java/ghostcat/capstone/image_query/ImageQueryResponse.java
+++ src/main/java/ghostcat/capstone/image_query/ImageQueryResponse.java
@@ -3,10 +3,11 @@ package ghostcat.capstone.image_query;
 import ghostcat.capstone.holders.Image;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ImageQueryResponse {
-   public ArrayList<Image> images = new ArrayList<>();
+   public List<Image> images = new ArrayList<>();
    public String errorMsg = "";
    public boolean success = true;
 }
